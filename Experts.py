184a19c33b961f467154ec3b506334b413e0e4f2
morsy <morsyb.6c@gmail.com>
2022-05-31 17:53:20 +0300

    Agent

---
+++ Experts.py
@@ -0,0 +1,61 @@
+from RoshamboPlayer import RoshamboPlayer
+
+
+class Urza(RoshamboPlayer):
+    def __init__(self):
+        pass
+
+    def newGame(self, trial):
+        pass
+
+    def storeMove(self, move, score):
+        pass
+
+    def nextMove(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getAuthor(self):
+        pass
+
+
+class megaHal(RoshamboPlayer):
+    def __init__(self):
+        pass
+
+    def newGame(self, trial):
+        pass
+
+    def storeMove(self, move, score):
+        pass
+
+    def nextMove(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getAuthor(self):
+        pass
+
+
+class Greenberg(RoshamboPlayer):
+    def __init__(self):
+        pass
+
+    def newGame(self, trial):
+        pass
+
+    def storeMove(self, move, score):
+        pass
+
+    def nextMove(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getAuthor(self):
+        pass
--- Tournament.py
+++ Tournament.py
@@ -1,21 +1,55 @@
 from Dummies import *
+from Agent import *
 
 
 class Tournament:
     def __init__(self):
         self.participants = []
+        self.score = []
         self.addPlayers()
+        self.matchHistory = []
 
     def addPlayers(self):
-        self.participants(AntiFlat())
-        self.participants(Copy())
-        self.participants(Freq())
-        self.participants(Flat())
-        self.participants(Foxtrot())
-        self.participants(Bruijn81())
-        self.participants(Pi())
-        self.participants(Play226())
-        self.participants(RndPlayer())
-        self.participants(Rotate())
-        self.participants(Switch())
-        self.participants(SwitchALot())
+        self.participants.append(Agent())
+        self.participants.append(AntiFlat())
+        self.participants.append(Copy())
+        self.participants.append(Freq())
+        self.participants.append(Flat())
+        self.participants.append(Foxtrot())
+        self.participants.append(Bruijn81())
+        self.participants.append(Pi())
+        self.participants.append(Play226())
+        self.participants.append(RndPlayer())
+        self.participants.append(Rotate())
+        self.participants.append(Switch())
+        self.participants.append(SwitchALot())
+        for _ in range(len(self.participants)):
+            self.score.append(0)
+
+    def start(self):
+        for player1 in range(len(self.participants) - 1):
+            player1History = []
+            for player2 in range(player1 + 1, len(self.participants)):
+                finalScore = self.startGame(player1, player2)
+                player1History.append(finalScore)
+            self.matchHistory.append(player1History)
+
+    def startGame(self, player1, player2):
+        score1, score2 = 0, 0
+        for _ in range(1000):
+            move1 = self.participants[player1].nextMove()
+            move2 = self.participants[player2].nextMove()
+            if (move1 == 0 and move2 == 1) or (move1 == 1 and move2 == 2) or (move1 == 2 and move2 == 0):
+                score2 += 1
+                score1 -= 1
+
+            if (move2 == 0 and move1 == 1) or (move2 == 1 and move1 == 2) or (move2 == 2 and move1 == 0):
+                score1 += 1
+                score2 -= 1
+
+        self.score[player1] += score1
+        self.score[player2] += score2
+
+        if score2 > score1:
+            return -1
+        return 1
